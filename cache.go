8b02100bd8f1183aaba1f84086b00afce7e4df0b
Marek Denis <marek.denis@gmail.com>
2016-01-18 18:05:57 +0100

    Add priority queue for events stats.

--- cache.go
+++ cache.go
@@ -5,11 +5,16 @@ import (
 	"sync"
 )
 
+const HeapLenght = 10
+
 type Entry struct {
 	Id           string
 	TotalHits    uint64
 	StatusCodes  map[int]uint64
 	MethodsStats map[string]uint64
+
+	// used by heap.Interface
+	index int
 }
 
 func NewEntry() *Entry {
@@ -17,6 +22,7 @@ func NewEntry() *Entry {
 		"", 0,
 		make(map[int]uint64),
 		make(map[string]uint64),
+		0,
 	}
 }
 
@@ -28,39 +34,56 @@ func (e *Entry) Stringer() string {
 type Cache struct {
 	m      sync.Mutex
 	Memory map[string]*Entry
+	Pq     PriorityQueue
 }
 
 func NewCache() *Cache {
 	c := new(Cache)
 	c.Memory = make(map[string]*Entry)
+	// initialize priority queue
+	c.Pq = make(PriorityQueue, 0, HeapLenght)
 	return c
 }
 
-func store(storage *Cache, queue chan *Log) {
+func Store(storage *Cache, queue chan *Log) {
 
 	for l := range queue {
 
 		storage.m.Lock()
 
 		var e *Entry = nil
+		var isNew bool
 		if _, ok := storage.Memory[l.Section]; ok == false {
-			fmt.Println("CACHE MISS")
+
 			e = NewEntry()
 			e.Id = l.Section
 			storage.Memory[l.Section] = e
+			isNew = true
+
 		} else {
-			fmt.Println("CACHE HIT")
+			isNew = false
 			e = storage.Memory[l.Section]
 		}
+
+		// update counters
 
 		e.StatusCodes[l.Status]++
 		e.MethodsStats[l.Method]++
 		e.TotalHits++
 
+		// update priority queue
+
+		if isNew && storage.Pq.Len() < HeapLenght {
+			storage.Pq = append(storage.Pq, e)
+		} else if isNew {
+			heap.Push(&storage.Pq, e)
+			storage.Pq.update(e)
+			for storage.Pq.Len() > HeapLenght {
+				storage.Pq.Pop()
+			}
+		}
+
 		fmt.Println(e.Stringer())
-
-		fmt.Println(storage)
-
 		storage.m.Unlock()
 	}
 }
---
+++ pq.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"container/heap"
+)
+
+type PriorityQueue []*Entry
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// Condition for implementing minheap
+	return pq[i].TotalHits < pq[j].TotalHits
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Entry)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Entry in the queue.
+func (pq *PriorityQueue) update(item *Entry) {
+	heap.Fix(pq, item.index)
+}
